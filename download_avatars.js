c4438477e47aba1bf3af67b36abc174cca94e8d5
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-01-14 22:31:09 +0000

    support command line arguments and make them required

--- download_avatars.js
+++ download_avatars.js
@@ -1,47 +1,55 @@
-var request = require('request');
-var fs = require('fs');
-var secret  = require('./secrets.js')
-var GITHUB_TOKEN = secret['GITHUB_TOKEN'];;
-console.log('Welcome to the GitHub Avatar Downloader!');
+var args = process.argv.slice(2);
+var owner = args[0];
+var repo = args[1];
+
+if(owner === undefined || repo === undefined){
+  console.log("Error not enough arugemnts");
+} else {
+  var request = require('request');
+  var fs = require('fs');
+  var secret  = require('./secrets.js')
+  var GITHUB_TOKEN = secret['GITHUB_TOKEN'];;
+  console.log('Welcome to the GitHub Avatar Downloader!');
 
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': GITHUB_TOKEN
-    }
-  };
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': GITHUB_TOKEN
+      }
+    };
 
-  request(options, function(err, res, body) {
-    var data = JSON.parse(body);
-    cb(err, data);
+    request(options, function(err, res, body) {
+      var data = JSON.parse(body);
+      cb(err, data);
+    });
+  }
+
+
+
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .on('error', function(err){
+        throw err;
+    })
+
+    .on('end', function(){
+      console.log('Download Complete...');
+    })
+
+    .pipe(fs.createWriteStream(filePath));
+  }
+
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    result.forEach(function(element){
+      var url = element['avatar_url'];
+      var path = element['login'];
+      downloadImageByURL(url,path);
+
+    });
   });
 }
-
-
-
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-  .on('error', function(err){
-      throw err;
-  })
-
-  .on('end', function(){
-    console.log('Download Complete...');
-  })
-
-  .pipe(fs.createWriteStream(filePath));
-}
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  result.forEach(function(element){
-    var url = element['avatar_url'];
-    var path = element['login'];
-    downloadImageByURL(url,path);
-
-  });
-});
