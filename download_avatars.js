ee593e48c455a398692e35cac70553622d8f82f3
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-01-14 22:19:09 +0000

    Implement downloadImageByURL and connected functions downloaded images

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var secret  = require('./secrets.js')
 var GITHUB_TOKEN = secret['GITHUB_TOKEN'];;
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -19,9 +20,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function(err){
+      throw err;
+  })
+
+  .on('end', function(){
+    console.log('Download Complete...');
+  })
+
+  .pipe(fs.createWriteStream(filePath));
+}
+
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   result.forEach(function(element){
-    console.log(element['avatar_url']);
+    var url = element['avatar_url'];
+    var path = element['login'];
+    downloadImageByURL(url,path);
+
   });
 });
