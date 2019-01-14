bf2b892716d068ff6e79a08b9033d1d3fae682b3
Anisa Tahlil <anisa.tahlil22@gmial.com>
2019-01-14 21:45:55 +0000

    added authorization header

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,22 @@
 var request = require('request');
-
+var secret  = require('./secrets.js')
+var GITHUB_TOKEN = secret['GITHUB_TOKEN'];;
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-}
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
